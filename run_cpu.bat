ffb87c4e6ddfce0ab154df1aa0d1b0526140ddbd
ahaoboy <504595380@qq.com>
2025-04-04 14:53:19 +0800

    doc

--- run_cpu.bat
+++ run_cpu.bat
@@ -10,23 +10,22 @@ set "streamlit_path=%script_dir%python_cpu\Scripts\streamlit.exe"
 set "python_path=%script_dir%python_cpu\python.exe"
 
 if exist "%pip_path%" (
-    echo pip ok
+    @REM echo pip ok
 ) else (
-    echo install pip
+    @REM echo install pip
     "%python_path%" "%get_pip_path%"
 )
 
 if exist "%streamlit_path%" (
-    echo streamlit ok
+    @REM echo streamlit ok
 ) else (
-    echo install streamlit
+    @REM echo install streamlit
     "%python_path%" -m pip install streamlit
 )
 
-cd /d %%~dp0
 
-cd zundamon-speech-webui
+cd zundamon-speech-webui\GPT-SoVITS
 
-"%~dp0python_cpu\python.exe" zundamon_speech_run.py
+"%streamlit_path%" run zundamon_webui.py
 
 pause
