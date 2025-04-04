766db05477fa1c8a3b921724d5a80f8c6778a24a
ahaoboy <504595380@qq.com>
2025-04-04 18:24:52 +0800

    tree

--- tree.ps1
+++ tree.ps1
@@ -1,16 +1,15 @@
-$PSDefaultParameterValues['Out-Default:Width'] = 4096
 Get-ChildItem -Recurse | Where-Object { !$_.PSIsContainer } | Select-Object @{
-    Name="RelativePath";
-    Expression={$_.FullName.Substring((Get-Location).Path.Length).TrimStart('\').Replace('\','/')}
+    Name = "FullName";
+    Expression = { $_.FullName }
 }, @{
-    Name="Size";
-    Expression={
+    Name = "Size";
+    Expression = {
         $size = $_.Length
-        switch($size) {
-            {$_ -ge 1GB} { "{0:N2} GB" -f ($size/1GB); break }
-            {$_ -ge 1MB} { "{0:N2} MB" -f ($size/1MB); break }
-            {$_ -ge 1KB} { "{0:N2} KB" -f ($size/1KB); break }
+        switch ($size) {
+            { $_ -ge 1GB } { "{0:N2} GB" -f ($size / 1GB); break }
+            { $_ -ge 1MB } { "{0:N2} MB" -f ($size / 1MB); break }
+            { $_ -ge 1KB } { "{0:N2} KB" -f ($size / 1KB); break }
             default { "$size B" }
         }
     }
-}
+} | Export-Csv -Path "tree.csv" -Encoding UTF8 -NoTypeInformation
